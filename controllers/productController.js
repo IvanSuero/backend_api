a580656fba01cb36c43fe1cef175178909775103
Ivan Suero <ivan.suero@kettal.es>
2024-02-26 08:56:19 +0100

    fixed

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,16 +3,12 @@ const querys = require('../database/querys')
 
 const productsController = {
     async getAll(req, res) {
-        await dbFunction(querys.getAllProducts, res)
-    }
-}
-
-const dbFunction = (query, res) => {
-    try {
-        const [rows, fields] = pool.query(query);
-        res.json({ products: rows });
-    } catch (error) {
-        res.json({ error: error.message})
+        try {
+            const [rows, fields] = await pool.query(querys.getAllProducts);
+            res.json({ products: rows });
+        } catch (error) {
+            res.json({ error: error.message})
+        }
     }
 }
 
