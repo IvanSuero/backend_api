c0c502b21d99a44af0c837c3a32a4f603f4409b6
Ivan Suero <ivan.suero@kettal.es>
2024-02-26 08:41:14 +0100

    sending data

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,8 +3,8 @@ const pool = require('../database/dbConfig');
 const productsController = {
     async getAll(req, res) {
         try {
-            const [rows, fields] = await pool.query("SELECT * FROM products");
-            res.json({ products: rows, fields: fields});
+            const products = await pool.query("SELECT * FROM products");
+            res.json({ products: products });
         } catch (error) {
             console.error(error.message)
             res.json({ error: error.message})
