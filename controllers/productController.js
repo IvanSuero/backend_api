cb0529db8b79eedbdb9521a9a0807a1e93afceae
Ivan <ivan.suerop@gmail.com>
2024-03-10 11:43:01 +0100

    simplify app

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,33 +1,9 @@
-import { querys, querysWithParams } from '../database/querys'
+import { querys } from '../database/querys'
 import { callDbGetFunction, callDbGetFunctionWithParams } from '../database/dbCallFunction'
 
 const productController = {
     getAllProducts(req, res) {
-        callDbGetFunction(res, querys.getAllProducts)
-    },
-    getProductById(req, res) {
-        callDbGetFunctionWithParams(res, querysWithParams.getProductById, req.params)
-    },
-    getProductByName(req, res) {
-        callDbGetFunctionWithParams(res, querysWithParams.getProductByName, req.params)
-    },
-    getProductByStock(req, res) {
-        callDbGetFunctionWithParams(res, querysWithParams.getProductByStock, req.params)
-    },
-    getProductByBuyPrice(req, res) {
-        callDbGetFunctionWithParams(res, querysWithParams.getProductByBuyPrice, req.params)
-    },
-    getProductBySellPrice(req, res) {
-        callDbGetFunctionWithParams(res, querysWithParams.getProductBySellPrice, req.params)
-    },
-    getProductByVolume(req, res) {
-        callDbGetFunctionWithParams(res, querysWithParams.getProductByVolume, req.params)
-    },
-    getProductByWeight(req, res) {
-        callDbGetFunctionWithParams(res, querysWithParams.getProductByWeight, req.params)
-    },
-    getProductBySupplierId(req, res) {
-        callDbGetFunctionWithParams(res, querysWithParams.getProductBySupplierId, req.params)
+        callDbGetFunction(res, querys.querys.getAllProducts)
     }
 }
 
