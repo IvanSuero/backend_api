a3043ceb5e7429b19f5b69e63ce3cc6feb047aad
Ivan <ivan.suerop@gmail.com>
2024-03-10 11:45:14 +0100

    s

--- router/data.router.js
+++ router/data.router.js
@@ -5,13 +5,6 @@ const columns = require("../sampleColumns")
 const productController = require("../controllers/productController")
 
 router.get('/data/products', productController.getAllProducts)
-router.get('/data/products/:id', productController.getProductById)
-router.get('/data/products/:name', productController.getProductByName)
-router.get('/data/products/:stock', productController.getProductByStock)
-router.get('/data/products/:buyPrice', productController.getProductByBuyPrice)
-router.get('/data/products/:sellPrice', productController.getProductBySellPrice)
-router.get('/data/products/:volume', productController.getProductByVolume)
-router.get('/data/products/:weight', productController.getProductByWeight)
 
 router.get('/columns', (req, res) => res.json({columns: columns}))
 
