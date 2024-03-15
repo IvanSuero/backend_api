b547c3bec7f81caecaac9eff1e1ed9f9536761fe
Ivan Suero <ivan.suero@kettal.es>
2024-03-15 10:57:51 +0100

    eee

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,30 +6,30 @@ const productController = {
         callDbGetFunction(res, querys.getAllProducts)
     },
     getProductByParam(req, res) {
-        switch (req.body.param) {
+        switch (req.body.paramName) {
             case 'id':
-                callDbGetFunctionWithParams(res, 'getProductById', req.body.id)
+                callDbGetFunctionWithParams(res, 'getProductById', req.body.param)
                 break;
             case 'name':
-                callDbGetFunctionWithParams(res, 'getProductByName', req.body.name)
+                callDbGetFunctionWithParams(res, 'getProductByName', req.body.param)
                 break;
             case 'stock':
-                callDbGetFunctionWithParams(res, 'getProductByStock', req.body.stock)
+                callDbGetFunctionWithParams(res, 'getProductByStock', req.body.param)
                 break;
             case 'buy_price':
-                callDbGetFunctionWithParams(res, 'getProductByBuyPrice', req.body.buy_price)
+                callDbGetFunctionWithParams(res, 'getProductByBuyPrice', req.body.param)
                 break;
             case 'sell_price':
-                callDbGetFunctionWithParams(res, 'getProductBySellPrice', req.body.sell_price)
+                callDbGetFunctionWithParams(res, 'getProductBySellPrice', req.body.param)
                 break;
             case 'volume':
-                callDbGetFunctionWithParams(res, 'getProductByVolume', req.body.volume)
+                callDbGetFunctionWithParams(res, 'getProductByVolume', req.body.param)
                 break;
             case 'weight':
-                callDbGetFunctionWithParams(res, 'getProductByWeight', req.body.weight)
+                callDbGetFunctionWithParams(res, 'getProductByWeight', req.body.param)
                 break;
             case 'supplier_id':
-                callDbGetFunctionWithParams(res, 'getProductBySupplierId', req.body.supplier_id)
+                callDbGetFunctionWithParams(res, 'getProductBySupplierId', req.body.param)
                 break;
             default:
                 res.json({ error: 'Invalid parameter' })
