0a0710392370c6e9da9510fab96fc45298291edc
Ivan Suero <ivan.suero@kettal.es>
2024-03-15 09:14:24 +0100

    a

--- database/dbCallFunction.js
+++ database/dbCallFunction.js
@@ -13,7 +13,8 @@ const callDbGetFunction = async (res, query) => {
 
 const callDbGetFunctionWithParams = async (res, query, params) => {
   try {
-      const key = Object.keys(params)[0];      
+      const key = Object.keys(params)[0]
+      console.log(`${querysWithParams[query]} ${key} = '${params[key]}'`)
       const [rows, fields] = await pool.query(`${querysWithParams[query]} ${key} = '${params[key]}'`)
       res.json({ data: rows, key: key, value: params[key]});
   } catch (error) {
