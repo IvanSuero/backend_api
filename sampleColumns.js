9dcb9f14b0fc252a604bd6a277344f7fd46a13fe
Ivan Suero <ivan.suero@kettal.es>
2024-03-22 13:07:49 +0100

    visible columns

--- sampleColumns.js
+++ sampleColumns.js
@@ -5,49 +5,56 @@ export const columns = {
         label: 'Reference',
         field: 'reference',
         align: 'left',
-        sortable: true
+        sortable: true,
+        visible: true
       },
       {
         name: 'description',
         label: 'Description',
         field: 'description',
         align: 'left',
-        sortable: false
+        sortable: false,
+        visible: true
       },
       {
         name: 'buy_price',
         label: 'Buy Price',
         field: 'buy_price',
         align: 'left',
-        sortable: true
+        sortable: true,
+        visible: false
       },
       {
         name: 'sell_price',
         label: 'Sell Price',
         field: 'sell_price',
         align: 'left',
-        sortable: true
+        sortable: true,
+        visible: false
       },
       {
         name: 'stock',
         label: 'Stock',
         field: 'stock',
         align: 'left',
-        sortable: true
+        sortable: true,
+        visible: true
       },
       {
         name: 'volume',
         label: 'Volume',
         field: 'volume',
         align: 'left',
-        sortable: true
+        sortable: true,
+        visible: false
       },
       {
         name: 'weight',
         label: 'Weight',
         field: 'weight',
         align: 'left',
-        sortable: true
+        sortable: true,
+        visible: false
       }
     ]
 }
